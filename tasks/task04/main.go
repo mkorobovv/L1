b5f8c8f9de21c7a8110e052c207854a2df462141
Maksim Korobov <korobov_m04@mail.ru>
2023-12-22 23:31:11 +0300

    refactor task 4

--- tasks/task04/main.go
+++ tasks/task04/main.go
@@ -1,18 +1,43 @@
 package main
 
-import "fmt"
-
-func wahttype(unknown interface{}) {
-	fmt.Printf("%T\n", unknown)
-}
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-	var i int
-	var b bool
-	var s string
-	var f float32
-	wahttype(i)
-	wahttype(b)
-	wahttype(s)
-	wahttype(f)
+	mainChan := make(chan int)
+
+	var nWorkers int
+	fmt.Print("Введите количество воркеров: ")
+	fmt.Scan(&nWorkers)
+
+	info := make(chan os.Signal, 1)
+	signal.Notify(info, syscall.SIGINT, os.Interrupt)
+
+	for i := 1; i <= nWorkers; i++ {
+		go woker(mainChan, i)
+	}
+
+	for {
+		select {
+		case <-info:
+			close(mainChan)
+			close(info)
+			return
+		default:
+			mainChan <- rand.Intn(100)
+		}
+	}
+}
+
+func woker(ch <-chan int, wid int) {
+	for v := range ch {
+		fmt.Printf("Получено число %d из канала от воркера %d\n", v, wid)
+		time.Sleep(2 * time.Second)
+	}
 }
